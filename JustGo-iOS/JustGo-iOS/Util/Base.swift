18c05d19b128464e06760e7e622abdccd4b3dea7
Woopa01 <whdnwls49@naver.com>
2019-05-16 23:28:38 +0900

    initial commit

---
+++ JustGo-iOS/JustGo-iOS/Util/Base.swift
@@ -0,0 +1,9 @@
+//
+//  Base.swift
+//  JustGo-iOS
+//
+//  Created by 조우진 on 16/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
---
+++ JustGo-iOS/JustGo-iOS/ViewControllers/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustGo-iOS
+//
+//  Created by 조우진 on 16/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
