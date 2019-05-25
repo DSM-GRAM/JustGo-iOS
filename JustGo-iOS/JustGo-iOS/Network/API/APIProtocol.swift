441b4f2459dc94e9933b425e6b72ea38b04fc4e1
donkyey <machun2002@naver.com>
2019-05-25 16:16:49 +0900

    [Add] API Protocol

---
+++ JustGo-iOS/JustGo-iOS/Network/API/APIProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  JustGo-iOS
+//
+//  Created by leedonggi on 25/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+
+protocol API {
+    
+    func getPath() -> String
+    
+}
