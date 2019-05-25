c4e571aad87792e69c78b59b04013b54ab081264
donkyey <machun2002@naver.com>
2019-05-25 16:11:55 +0900

    [Add] Podfile

--- JustGo-iOS/JustGo-iOS/Network/Connector.swift
+++ JustGo-iOS/JustGo-iOS/Network/Connector.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+import RxSwift
+import RxAlamofire
+import Alamofire
+
+public class Connector{
+    
+    public static let instance = Connector()
+    
+    private let baseUrl = "baseUrl"
+    private init(){ }
+    
+    func getRequest(_ subPath: API, method: RequestMethod, params: [String : Any]? = nil) -> URLRequest{
+        var urlStr = basePath + subPath.getPath()
+        
+        if method == .get{
+            var query = params?.map{ "\($0)=\($1)" }.reduce(""){ f, s -> String in "\(f)&\(s)" } ?? ""
+            if !query.isEmpty{ query.removeFirst(); urlStr += ("?" + query) }
+        }
+        
+        var request = URLRequest(url: URL(string: urlStr)!)
+        request.httpMethod = method.rawValue
+        
+        if method != .get, params != nil{
+            let jsonData = try? JSONSerialization.data(withJSONObject: params!)
+            request.httpBody = jsonData
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        
+        return request
+    }
+    
+}
+
+public extension URLRequest{
+    
+    func emptyData(vc: UIViewController) -> Observable<Int>{
+        return decodeData(String.self, vc: vc).map{ $0.0 }
+    }
+    
+    func decodeData<T>(_ type: T.Type, vc: UIViewController? = nil) -> Observable<(Int, T?)> where T: Decodable{
+        return requestData(self)
+            .single()
+            .map{ ($0.0.statusCode, $0.1) }
+            .map{ (code, data) in
+                let decodeData = try? JSONDecoder().decode(type, from: data)
+                return (code, decodeData)
+            }
+            .filter{ (code, _) in
+                guard let vc = vc else { return true }
+                if code == 500{ print("서버 오류") }
+                return code != 500
+        }
+    }
+    
+}
+
+public enum RequestMethod: String{
+    
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    
+}
---
+++ JustGo-iOS/Pods/Target Support Files/Pods-JustGo-iOS/Pods-JustGo-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JustGo_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_JustGo_iOS
+@end
